ba0999fc4b5647934508d5645580de543554d164
marinachow <chow.marina@gmail.com>
2024-02-21 11:02:11 +0100

    Created search component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import Search from './Search';
 
-function App() {
+const App = () => {
+  const handleSearch = (searchTerm) => {
+    console.log('Searching for:', searchTerm);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Wifi API Interface</h1>
+      <Search onSearch={handleSearch} />
     </div>
   );
-}
+};
 
 export default App;

13f4129f6fe02e390566de6864f036f5733e77fa
marinachow <chow.marina@gmail.com>
2024-02-21 14:56:44 +0100

    Create create_database.sql

---
+++ create_database.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS wiki_db;
+
+USE wiki_db;
+
+CREATE TABLE IF NOT EXISTS ProgrammingLanguages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    language_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS SearchResults (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    language_id INT,
+    title VARCHAR(255) NOT NULL,
+    snippet TEXT NOT NULL,
+    FOREIGN KEY (language_id) REFERENCES ProgrammingLanguages(id)
+);

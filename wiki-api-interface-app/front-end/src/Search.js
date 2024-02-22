29d62bc3383c831f066ee5e3e2972ab2ce323e96
marinachow <chow.marina@gmail.com>
2024-02-22 08:37:51 +0100

    added message

--- wiki-api-interface-app/front-end/src/Search.js
+++ wiki-api-interface-app/front-end/src/Search.js
@@ -1,4 +1,3 @@
-// Search.js
 import React, { useState } from 'react';
 import axios from 'axios';
 
@@ -45,52 +44,63 @@ const Search = ({ onSearch }) => {
   };  
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Search..."
-        value={searchTerm}
-        onChange={handleChange}
-      />
-      <label>
-        Filter by date:
-        <input
-          type="date"
-          value={filterMinDate}
-          onChange={handleMinDateChange}
-        />
-        <input
-          type="date"
-          value={filterMaxDate}
-          onChange={handleMaxDateChange}
-        />
-      </label>
-      <label>
-        Filter by word count:
+    <div>
+      <p>Welcome to Wiki API interface! Our database is not fully populated yet, but you may search for any of the following these terms:</p>
+      <ul>
+        <li>Python</li>
+        <li>Java</li>
+        <li>JavaScript</li>
+        <li>Ruby</li>
+        <li>Go</li>
+        <li>Swift</li>
+      </ul>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
-          placeholder="Min word count"
-          value={filterMinWordCount}
-          onChange={handleMinWordCountChange}
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleChange}
         />
-        <input
-          type="text"
-          placeholder="Max word count"
-          value={filterMaxWordCount}
-          onChange={handleMaxWordCountChange}
-        />
-      </label>
-      <label>
-        Sort by:
-        <select value={sortBy} onChange={handleSortChange}>
-          <option value="">None</option>
-          <option value="alphabetical">Alphabetical</option>
-          <option value="date">Date</option>
-          <option value="wordcount">Word Count</option>
-        </select>
-      </label>
-      <button type="submit">Search</button>
-    </form>
+        <label>
+          Filter by date:
+          <input
+            type="date"
+            value={filterMinDate}
+            onChange={handleMinDateChange}
+          />
+          <input
+            type="date"
+            value={filterMaxDate}
+            onChange={handleMaxDateChange}
+          />
+        </label>
+        <label>
+          Filter by word count:
+          <input
+            type="text"
+            placeholder="Min word count"
+            value={filterMinWordCount}
+            onChange={handleMinWordCountChange}
+          />
+          <input
+            type="text"
+            placeholder="Max word count"
+            value={filterMaxWordCount}
+            onChange={handleMaxWordCountChange}
+          />
+        </label>
+        <label>
+          Sort by:
+          <select value={sortBy} onChange={handleSortChange}>
+            <option value="">None</option>
+            <option value="alphabetical">Alphabetical</option>
+            <option value="date">Date</option>
+            <option value="wordcount">Word Count</option>
+          </select>
+        </label>
+        <button type="submit">Search</button>
+      </form>
+    </div>
   );
 };
 
